01472f5cd470a374535fc61240b9fd3738b1792c
Rohan Kumar <kumar2003rohan@gmail.com>
2024-04-06 15:53:46 +0530

    Firebase Setup

---
+++ src/firebase/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDCwkkjR_ZUT3FaN6kdPT5kwzZrIZCMcQw",
+  authDomain: "velocity-mart.firebaseapp.com",
+  projectId: "velocity-mart",
+  storageBucket: "velocity-mart.appspot.com",
+  messagingSenderId: "78196448859",
+  appId: "1:78196448859:web:e566f25d975bf98a2ce97e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
