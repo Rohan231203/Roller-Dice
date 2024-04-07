9cc0d7e05e45fe778ef038e42f59aabf8da3343a
Aytaditya <adityaaryan531@gmail.com>
2024-04-07 14:00:52 +0530

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,11 @@ import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <>
-<<<<<<< HEAD
-     <div className="navbar bg-blue-400"></div>
-     <div className="flex">
-        <div className="sidebar flex flex-col items-center text-white w-1/4 h-screen">Hi</div>
-        <div className="content bg-slate-400 text-white w-3/4 h-screen">this is content</div>
-     </div>
-=======
       <LogInNav />
       <SignUpNav />
       <Footer />
       <Login />
       <Toaster />
->>>>>>> 9208722120a95c107f4af4f3141f3b1165eadacd
     </>
   );
 }
