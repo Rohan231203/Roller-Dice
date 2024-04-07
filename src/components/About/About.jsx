3212abb2b5acbafcc88eeca4fef91ef8c0a40787
UnknownRider <barikpriyam28@gmail.com>
2024-04-08 03:06:36 +0530

    Item List

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-// import React from 'react'
-
-export default function about() {
-    return (
-        <>
-            <section className="flex flex-col justify-center items-center bg-gray-200 py-20">
-                <div className="about-content text-center max-w-3xl">
-                    <h2 className="text-3xl font-bold mb-4">About Us</h2>
-                    <p className="text-lg mb-8">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at libero sed turpis mattis dictum. Nulla facilisi. Sed quis est ac ligula aliquet tincidunt. Vestibulum placerat luctus neque at ultricies. Ut vitae urna eu justo pharetra vestibulum vel ut lectus. Sed eget odio sit amet sapien mattis fermentum ac sed odio. Nam vulputate risus in lacinia suscipit.
-                    </p>
-                    <p className="text-lg">
-                        Phasellus vestibulum urna vel odio vehicula, vel placerat nisl congue. Morbi fermentum nisi at justo tristique vehicula. Integer dapibus diam nec augue tempor, et vehicula orci blandit. Vestibulum a ex vitae turpis tristique lobortis. Aenean id dui leo. Duis viverra nisl sit amet quam consequat vestibulum. Integer quis justo ut sem malesuada congue et eget turpis.
-                    </p>
-                </div>
-            </section></>
-    )
-}
-=======
 import React from 'react'
 
 function About() {
@@ -65,4 +45,3 @@ function About() {
 }
 
 export default About
->>>>>>> 1137518888c07eb4e2c3c160e1c691115c0ab192
