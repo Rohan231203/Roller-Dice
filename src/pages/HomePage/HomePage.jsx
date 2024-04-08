a185cb2726de65399edaddf2f9fa30086ed1934d
Aytaditya <adityaaryan531@gmail.com>
2024-04-09 02:05:26 +0530

    about page created

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,9 +2,9 @@
 import LogInNav from "../../components/LogInNav/LogInNav";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Footer from "../../components/Footer/Footer";
-import About from '../../components/About/About';
+
 import HomeSection from "../../components/HomeSection/HomeSection";
-import About from "../../components/About/About";
+
 
 export default function HomePage() {
   return (
@@ -17,14 +17,5 @@ export default function HomePage() {
       <Footer />
     </>
   );
-    <div>
-      <LogInNav/>
-      <div className="main-section-home flex">
-      <Sidebar/>
-      <HomeSection />
-      </div>
-      <Footer/>
-      <About/>
-    </div>
-  )
+  
 }
