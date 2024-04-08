a4681e548637068d0ff20f77bc2be73158525330
UnknownRider <barikpriyam28@gmail.com>
2024-04-08 13:06:21 +0530

    User Authentication Done

---
+++ src/pages/ProfilePage/Profilepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProfileSection from '../../components/ProfileSection/ProfileSection.jsx'
+// import ProfileSection from "./components/ProfileSection/ProfileSection";
+
+
+export default function Profilepage() {
+  return (
+    <div>
+      <ProfileSection/>
+    </div>
+  )
+}
