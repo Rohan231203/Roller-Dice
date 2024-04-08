61a6b9ae30ca23a4f1802cb7167e13013417dfc9
akshanshsingh00 <akshanshsingh00@gmail.com>
2024-04-08 19:27:42 +0530

    Homesection half done

---
+++ src/components/HomeSection/HomeSection.jsx
@@ -0,0 +1,109 @@
+import { GiElectricalResistance } from "react-icons/gi";
+import { CgGym } from "react-icons/cg";
+import { IoBookSharp } from "react-icons/io5";
+import { FaBucket } from "react-icons/fa6";
+import { FaCartPlus } from "react-icons/fa6";
+
+const HomeSection = () => {
+  const categoryList = [
+    {
+      title: "Electrical and Mechanical components",
+      img: <GiElectricalResistance size={30} />,
+    },
+    {
+      title: "Sports and gym",
+      img: <CgGym size={30} />,
+    },
+    {
+      title: "Academic",
+      img: <IoBookSharp size={30} />,
+    },
+    {
+      title: "Amenities",
+      img: <FaBucket size={30} />,
+    },
+    {
+      title: "Others",
+      img: <FaCartPlus size={30} />,
+    },
+  ];
+  return (
+    <main
+      className="w-5/6 overflow-y-scroll flex flex-col"
+      style={{ height: "calc(100vh - var(--navbar-height))" }}
+    >
+      <div className="carousel min-h-[300px]">
+        <div className="carousel-item flex">
+          <img
+            src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg"
+            alt="Burger"
+            className="w-full h-auto"
+          />
+        </div>
+        <div className="carousel-item flex">
+          <img
+            src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg"
+            alt="Burger"
+            className="w-full h-auto"
+          />
+        </div>
+        <div className="carousel-item flex">
+          <img
+            src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg"
+            alt="Burger"
+            className="w-full h-auto"
+          />
+        </div>
+        <div className="carousel-item flex">
+          <img
+            src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg"
+            alt="Burger"
+            className="w-full h-auto"
+          />
+        </div>
+        <div className="carousel-item flex">
+          <img
+            src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg"
+            alt="Burger"
+            className="w-full h-auto"
+          />
+        </div>
+        <div className="carousel-item flex">
+          <img
+            src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg"
+            alt="Burger"
+            className="w-full h-auto"
+          />
+        </div>
+        <div className="carousel-item flex">
+          <img
+            src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg"
+            alt="Burger"
+            className="w-full h-auto"
+          />
+        </div>
+      </div>
+
+      <div className="categories w-full bg-white px-[2rem] py-[2rem] m-0">
+        <h2 className="text-2xl font-bold text-black mb-[2rem]">
+          Browse by categories
+        </h2>
+        <div className="categories-container flex justify-between">
+          {categoryList.map((catItem, index) => {
+            return (
+              <div
+                className="catItem border-[2.5px] border-black h-[10rem] w-[10rem] text-black text-lg font-bold flex flex-col gap-3 justify-center items-center text-center hover:bg-red-500 hover:text-white hover:border-red-500 cursor-pointer"
+                key={index}
+              >
+                <h2>{catItem.title}</h2>
+                {catItem.img}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default HomeSection;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,16 @@
 import LogInNav from "../../components/LogInNav/LogInNav";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Footer from "../../components/Footer/Footer";
+import HomeSection from "../../components/HomeSection/HomeSection";
 
 export default function HomePage() {
   return (
     <>
       <LogInNav />
-      <Sidebar />
+      <div className="main-section flex">
+        <Sidebar />
+        <HomeSection />
+      </div>
       <Footer />
     </>
   );
